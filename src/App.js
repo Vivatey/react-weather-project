c8e5196b549e26aaa00cfcf655116185a2e0f748
Vivatey <vivatey@gmail.com>
2023-04-13 17:49:44 +0100

    Added the GitHub repo URL link.

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by Viv and is{" "}
+        <a
+          href="https://github.com/Vivatey/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
